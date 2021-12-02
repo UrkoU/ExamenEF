1de1513be3db38177bbdf8c205b53cfb4e337aa8
UrkoU <ikbqa@plaiaundi.net>
2021-12-02 11:20:33 +0100

    Examen hecho (?)

---
+++ Migrations/20211202080901_Inicial.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFExamen.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alumnos",
+                columns: table => new
+                {
+                    AlumnoID = table.Column<int>(type: "int", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    Efectivo = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Pelo = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alumnos", x => x.AlumnoID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Modulos",
+                columns: table => new
+                {
+                    ModuloID = table.Column<int>(type: "int", nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Creditos = table.Column<int>(type: "int", nullable: false),
+                    Curso = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modulos", x => x.ModuloID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Matriculas",
+                columns: table => new
+                {
+                    MatriculaID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AlumnoID = table.Column<int>(type: "int", nullable: false),
+                    ModuloID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matriculas", x => x.MatriculaID);
+                    table.ForeignKey(
+                        name: "FK_Matriculas_Alumnos_AlumnoID",
+                        column: x => x.AlumnoID,
+                        principalTable: "Alumnos",
+                        principalColumn: "AlumnoID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Matriculas_Modulos_ModuloID",
+                        column: x => x.ModuloID,
+                        principalTable: "Modulos",
+                        principalColumn: "ModuloID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matriculas_AlumnoID_ModuloID",
+                table: "Matriculas",
+                columns: new[] { "AlumnoID", "ModuloID" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matriculas_ModuloID",
+                table: "Matriculas",
+                column: "ModuloID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Matriculas");
+
+            migrationBuilder.DropTable(
+                name: "Alumnos");
+
+            migrationBuilder.DropTable(
+                name: "Modulos");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,38 +11,63 @@ using static System.Console;
 public class InstitutoContext : DbContext
 {
 
+    public DbSet<Alumno> Alumnos { get; set; }
+    public DbSet<Matricula> Matriculas { get; set; }
+    public DbSet<Modulo> Modulos { get; set; }
     public string connString { get; private set; }
 
     public InstitutoContext()
     {
-        var database = ""; // "EF{XX}Nombre" => EF00Santi
+        var database = "EF13Urko"; // "EF{XX}Nombre" => EF00Santi
         connString = $"Server=185.60.40.210\\SQLEXPRESS,58015;Database={database};User Id=sa;Password=Pa88word;MultipleActiveResultSets=true";
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Matricula>().HasIndex(m => new
+        {
+            m.AlumnoID,
+            m.ModuloID
+        }).IsUnique();
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlServer(connString);
 
+
+
 }
 public class Alumno
 {
     [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
-    public int AlumnoId { get; set; }
-    /*
+    public int AlumnoID { get; set; }
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+    public decimal Efectivo { get; set; }
+    public string Pelo { get; set; }
+    public override string ToString() => $"{AlumnoID} con nombre {Nombre}, de {Edad} años y de pelo {Pelo} tiene {Efectivo} dinero";
 
-    */
 }
 public class Modulo
 {
     [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
-    public int ModuloId { get; set; }
-    /*
+    public int ModuloID { get; set; }
+    public string Titulo { get; set; }
+    public int Creditos { get; set; }
+    public int Curso { get; set; }
 
-    */
-
+    public override string ToString() => $"{ModuloID} con título {Titulo} de {Creditos} créditos en {Curso} curso";
 }
 public class Matricula
 {
+    [Key]
+    public int MatriculaID { get; set; }
+    public int AlumnoID { get; set; }
+    public int ModuloID { get; set; }
+    public Alumno alumno { get; set; }
+    public Modulo modulo { get; set; }
 
+    public override string ToString() => $"{MatriculaID} del alumno {AlumnoID} y módulo {ModuloID}";
 }
 
 class Program
@@ -52,31 +77,117 @@ class Program
         using (var db = new InstitutoContext())
         {
             // Borrar todo
+            // Borrar todo
+            db.Alumnos.RemoveRange(db.Alumnos);
+            db.Modulos.RemoveRange(db.Modulos);
+            db.Matriculas.RemoveRange(db.Matriculas);
 
+            db.SaveChanges();
             // Añadir Alumnos
+            Console.WriteLine("Añadir alumnos");
             // Id de 1 a 7
-
+            db.Alumnos.Add(new Alumno { AlumnoID = 1, Nombre = "Dani", Edad = 32, Efectivo = 5, Pelo = "Moreno" });
+            db.Add(new Alumno { AlumnoID = 2, Nombre = "Jenni", Edad = 27, Efectivo = 1, Pelo = "Castaño" });
+            db.Add(new Alumno { AlumnoID = 3, Nombre = "Yasminn", Edad = 25, Efectivo = 10, Pelo = "Moreno" });
+            db.Add(new Alumno { AlumnoID = 4, Nombre = "Javi", Edad = 20, Efectivo = 12, Pelo = "Moreno" });
+            db.Add(new Alumno { AlumnoID = 5, Nombre = "Unai", Edad = 20, Efectivo = 3, Pelo = "Castaño" });
+            db.Add(new Alumno { AlumnoID = 6, Nombre = "Asier", Edad = 19, Efectivo = 8, Pelo = "Moreno" });
+            db.Add(new Alumno { AlumnoID = 7, Nombre = "Jon", Edad = 32, Efectivo = 9, Pelo = "Moreno" });
             // Añadir Módulos
             // Id de 1 a 10
-
+            db.Add(new Modulo { ModuloID = 1, Titulo = "Gimnasia", Creditos = 6, Curso = 1 });
+            db.Add(new Modulo { ModuloID = 2, Titulo = "Euskera", Creditos = 12, Curso = 1 });
+            db.Add(new Modulo { ModuloID = 3, Titulo = "Inglés", Creditos = 12, Curso = 2 });
+            db.Add(new Modulo { ModuloID = 4, Titulo = "Latín", Creditos = 12, Curso = 2 });
+            db.Add(new Modulo { ModuloID = 5, Titulo = "Informática", Creditos = 6, Curso = 1 });
+            db.Add(new Modulo { ModuloID = 6, Titulo = "Castellano", Creditos = 9, Curso = 1 });
+            db.Add(new Modulo { ModuloID = 7, Titulo = "Administración de empresas", Creditos = 6, Curso = 2 });
+            db.Add(new Modulo { ModuloID = 8, Titulo = "Francés", Creditos = 6, Curso = 1 });
+            db.Add(new Modulo { ModuloID = 9, Titulo = "Física", Creditos = 9, Curso = 2 });
+            db.Add(new Modulo { ModuloID = 10, Titulo = "Química", Creditos = 9, Curso = 2 });
             // Matricular Alumnos en Módulos
 
+            foreach (Modulo modulo in db.Modulos)
+                foreach (Alumno alumno in db.Alumnos)
+                    db.Add(new Matricula
+                    {
+                        AlumnoID = alumno.AlumnoID,
+                        ModuloID = modulo.ModuloID
+                    });
+            db.SaveChanges();
         }
     }
 
     static void BorrarMatriculaciones()
     {
-        // Borrar las matriculas d
-        // AlumnoId multiplo de 3 y ModuloId Multiplo de 2;
-        // AlumnoId multiplo de 2 y ModuloId Multiplo de 5;
+        using (var db = new InstitutoContext())
+        {
+            // Borrar las matriculas de
+            // AlumnoId multiplo de 3 y ModuloId Multiplo de 2;
+            foreach (Matricula matricula in db.Matriculas)
+            {
+                if (matricula.AlumnoID % 3 == 0 && matricula.ModuloID % 2 == 0)
+                {
+                    db.Matriculas.Remove(matricula);
+                    Console.WriteLine("Matrícula de alumno " + matricula.AlumnoID + " y módulo " + matricula.ModuloID + " borrada ");
+                }
+                if (matricula.AlumnoID % 2 == 0 && matricula.ModuloID % 5 == 0)
+                {
+                    db.Matriculas.Remove(matricula);
+                    Console.WriteLine("Matrícula de alumno " + matricula.AlumnoID + " y módulo " + matricula.ModuloID + " borrada ");
+                }
+            }
+            db.SaveChanges();
+        }
     }
     static void RealizarQuery()
     {
         using (var db = new InstitutoContext())
         {
             // Las queries que se piden en el examen
+            Console.WriteLine("Order by de alumnos que tengan mucho dinero");
+            List<Alumno> AlumnosOrdenados = (db.Alumnos.OrderBy(o => o.Nombre).Where(o => o.Efectivo > 8)).ToList<Alumno>();
+            foreach (Alumno alumno in AlumnosOrdenados)
+            {
+                Console.WriteLine(alumno);
+            }
 
+            Console.WriteLine("Tipo anónimo");
+            var AlumnosAnonimos = db.Alumnos.Select(al => new
+            {
+                AlumnoID = al.AlumnoID,
+                Nombre = al.Nombre
+            }).OrderByDescending(al => al.AlumnoID);
+            foreach (var alumno in AlumnosAnonimos)
+            {
+                Console.WriteLine("ID: " + alumno.AlumnoID + " Nombre: " + alumno.Nombre);
+            }
+
+            // Console.WriteLine("Mostrar matriculaciones del penúltimo alumno");
+            // var al = db.Matriculas.GroupBy(
+            //     matricula => matricula.AlumnoID).
+            //     Select(g => new
+            //     {
+            //         AlumnoID = g.Key,
+            //         Matriculas = g.Count()
+            //     }).OrderByDescending(m => m.AlumnoID).Skip(1).First();
+            // Console.WriteLine("El alumno: " + al.AlumnoID.ToString() + " está matriculado en " + al.Matriculas + " módulos.");
+
+
+            // Console.WriteLine("Mostrar asignaturas de los alumnos");
+            // var ClasesDeAlumnos = (db.Matriculas.Join(db.Alumnos,
+            //     al => al.AlumnoID, matr => matr.AlumnoID, (matricula, alumno) => new
+            //     {
+            //         matricula.AlumnoID,
+            //         alumno.Nombre,
+            //         alumno.Edad
+            //     })).ToArray();
+            // foreach (var alumno in ClasesDeAlumnos)
+            // {
+            //     Console.WriteLine(alumno);
+            // }
         }
+
     }
 
     static void Main(string[] args)
